55af5f856024532d17462e97005502c5169f2d85
Hugo Marinho <hugomarinho@cpejr.com.br>
2022-04-20 21:22:50 -0300

    Criação de botões simples
    Apenas testando o Javascript em si.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 function App() {
+
+  const frase = "Olá mundo, em uma string!";
+
+  function alertClick(mensagem) {
+    alert(mensagem);
+  }
   return (
     <div>
       <h1>Olá mundo!</h1>
+      <h1>{frase}</h1>
+      <Title></Title>
+      <button onClick={() => alertClick("Primeiro botão")}>Botão 1</button>
+      <button onClick={() => alertClick("Segundo botão")}> Botão 2</button>
     </div>
   );
+}
+
+
+function Title() {
+  return(
+    <h1>Hello world!</h1>
+  )
 }
 
 export default App;
