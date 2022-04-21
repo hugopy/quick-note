e93f1437044ccf3d3f34084003a2adb994999139
Hugo Marinho <hugomarinho@cpejr.com.br>
2022-04-21 16:16:54 -0300

    Implementei useState para um formulario simples

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Membro from "./Membro";
 import Section from "./Section";
+import {useState} from "react";
 
 function App() {
+
+  const [name, setName] = useState();
+  const [cargo, setCargo] = useState();
 
   const Hugo = {
     nome: "Hugo",
@@ -23,6 +27,32 @@ function App() {
         <Membro membro={Hugo}/>
         <Membro membro={Davi}/>
       </Section>
+
+      <div>
+        <h1>Criar novo membro</h1>
+        
+        <input type="text" placeholder="Nome" onChange={(event) => setName(event.target.value)}></input>
+        
+        <br/>
+
+        <input id="GS" type="radio" name="cargo" value="Gerente Scrum"
+        onChange={(e) => setCargo(e.target.value)} />
+
+        <label for="GS">Gerente Scrum</label>
+
+        <br/>
+
+        <input id="GP" type="radio" name="cargo" value="Gerente de Produtos"
+        onChange={(e) => setCargo(e.target.value)} />
+
+        <label for="GP">Gerente de Produtos</label>
+
+        <br/>
+        <br/>
+
+        <p>Nome do membro: {name}</p>
+        <p>Cargo do membro: {cargo}</p>
+      </div>
     </div>
   );
 }
