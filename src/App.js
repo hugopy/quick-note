d768eedcd5abc9326c41e4442d207fb735d81be4
Hugo Marinho <hugomarinho@cpejr.com.br>
2022-04-21 17:04:00 -0300

    useState para manipular um array
    Também retirei warnings

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {useState} from "react";
 function App() {
 
   const [membro, setMembro] = useState({});
+  const [color, setColor] = useState();
+  const [carrinho, setCarrinho] = useState([]);
 
   function handleInputChange(e) {
     const key = e.target.name;
@@ -16,6 +18,21 @@ function App() {
     setMembro(newMembro);
     console.log(newMembro);
   }
+
+  function handleArrayChange(e) {
+    const value = e.target.value;
+    const newCarrinho = [...carrinho];
+    const index = carrinho.indexOf(value);
+
+    if(index === -1) {
+      newCarrinho.push(value);
+      setCarrinho(newCarrinho);
+    } else {
+      newCarrinho.splice(index, 1);
+      setCarrinho(newCarrinho);
+    }
+  }
+
   const Hugo = {
     nome: "Hugo",
     idade: 19,
@@ -43,7 +60,7 @@ function App() {
         
         <br/>
 
-        <input id="GS" name="cargo" type="radio" name="cargo" value="Gerente Scrum"
+        <input id="GS" name="cargo" type="radio" value="Gerente Scrum"
         onChange={handleInputChange} />
 
         <label for="GS">Gerente Scrum</label>
@@ -56,10 +73,41 @@ function App() {
         <label for="GP">Gerente de Produtos</label>
 
         <br/>
+
+        <input id="GP" type="radio" name="color" value="red"
+        onChange={ (e)=> {setColor(e.target.value)} } />
+
+        <label for="GP">red</label>
+
+        <br/>
+
+        <input id="GP" type="radio" name="color" value="blue"
+        onChange={ (e)=> {setColor(e.target.value)} } />
+
+        <label for="GP">blue</label>
+
+        <br/>
+        <input id="Tesoura" type="checkbox" value="Tesoura" onChange={handleArrayChange}></input>
+        <label htmlFor="Tesoura">Tesoura</label>
+        <br/>
+        <input id="Cola" type="checkbox" value="Cola" onChange={handleArrayChange}></input>
+        <label htmlFor="Cola">Cola</label>
+        <br/>
+        <input id = "Lápis" type="checkbox" value="Lápis" onChange={handleArrayChange}></input>
+        <label htmlFor="Lápis">Lápis</label>
+
+        <br/>
         <br/>
 
         <p>Nome do membro: {membro.nome}</p>
         <p>Cargo do membro: {membro.cargo}</p>
+      </div>
+      <div style={ {background: color} }>
+        <p>oi</p>
+      </div>
+
+      <div>
+        {carrinho.join(" ")}
       </div>
     </div>
   );
