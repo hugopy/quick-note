a756fa3fdd1ebef7a0234ebb9db415d84ad1b199
Hugo Marinho <hugomarinho@cpejr.com.br>
2022-04-26 19:04:03 -0300

    Desenvolvimento de HTML e CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Router from "./routes";
+import "./global.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return <Router/>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./login.css";
+import { Form, Button } from "react-bootstrap";
 
 export default function Login() {
-    return (
-        <Link to="/home">Clique para ir até a home!</Link>
-    );
+  return (
+    <>
+      <Link to="/home">Clique para ir até a home!</Link>
+      <div className="base">
+        <div className="container">
+          <h1>Quick Note</h1>
+          <Form className="inputs">
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Control type="email" placeholder="Email"/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="senha">
+              <Form.Control type="password" placeholder="Senha"/>
+            </Form.Group>
+            <Button variant="primary">Login</Button>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
 }
